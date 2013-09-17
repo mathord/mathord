2cae5bc058b324a06da3aa8f28c844d14e079849
mathord <mathord2013@gmail.com>
2013-09-17 14:52:25 +0800

    aaa

--- hello.c
+++ hello.c
@@ -1,8 +1,9 @@
+/* vim: set expandtab tabstop=4 softtabstop=4 shiftwidth=4: */
 
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-	printf("Hello World!\n");
-	return 0;
+    printf("Hello World!\n");
+    return 0;
 }

43c25a214264401e6778c4dce2c0eef97e37dbfc
mathord2013 <mathord2013@gmail.com>
2013-09-22 19:16:32 +0800

    aaa

---
+++ python/code.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import web
+
+render = web.template.render("templates/")
+db = web.database(dbn='mysql', user='root', pw='', db='db_test')
+
+urls = (
+	'/', 'index',
+	'/add', 'add'
+)
+
+class index:
+	def GET(self):
+		todos = db.select('todo')
+		return render.index(todos)
+
+class add:
+	def POST(self):
+		i = web.input()
+		n = db.insert('todo', title=i.title)
+		raise web.seeother('/')
+
+
+if __name__ == "__main__":
+	app = web.application(urls, globals())
+	app.run()
